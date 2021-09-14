470dd7618273f4ebfa6b40c6fe67c70a9aa043c7
Reshy <reshy@protonmail.com>
2021-09-14 11:24:34 +0200

    Change from function-based components to class-based

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React, { Component } from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,33 +1,43 @@
+import React, { Component } from 'react';
 import Button from './Button';
 import Title from './Title';
 import Results from './Results';
 
-const Calculator = () => (
-  <>
-    <Title value="Math Magicians" />
-    <div className="container">
-      <Results />
-      <Button type="but digit" value="AC" />
-      <Button type="but digit" value="+/-" />
-      <Button type="but digit" value="%" />
-      <Button type="but operator" value="+" />
-      <Button type="but digit" value="7" />
-      <Button type="but digit" value="8" />
-      <Button type="but digit" value="9" />
-      <Button type="but operator" value="x" />
-      <Button type="but digit" value="4" />
-      <Button type="but digit" value="5" />
-      <Button type="but digit" value="6" />
-      <Button type="but operator" value="-" />
-      <Button type="but digit" value="1" />
-      <Button type="but digit" value="2" />
-      <Button type="but digit" value="3" />
-      <Button type="but operator" value="+" />
-      <Button type="but digit double" value="0" />
-      <Button type="but digit" value="." />
-      <Button type="but operator" value="=" />
-    </div>
-  </>
-);
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <>
+        <Title value="Math Magicians" />
+        <div className="container">
+          <Results />
+          <Button type="but digit" value="AC" />
+          <Button type="but digit" value="+/-" />
+          <Button type="but digit" value="%" />
+          <Button type="but operator" value="+" />
+          <Button type="but digit" value="7" />
+          <Button type="but digit" value="8" />
+          <Button type="but digit" value="9" />
+          <Button type="but operator" value="x" />
+          <Button type="but digit" value="4" />
+          <Button type="but digit" value="5" />
+          <Button type="but digit" value="6" />
+          <Button type="but operator" value="-" />
+          <Button type="but digit" value="1" />
+          <Button type="but digit" value="2" />
+          <Button type="but digit" value="3" />
+          <Button type="but operator" value="+" />
+          <Button type="but digit double" value="0" />
+          <Button type="but digit" value="." />
+          <Button type="but operator" value="=" />
+        </div>
+      </>
+    );
+  }
+}
 
 export default Calculator;
