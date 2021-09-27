d1844dba689f8a705db6f1fe14279e2c1f7ba53b
Reshy <reshy@protonmail.com>
2021-09-27 11:58:21 +0200

    Fix deploy bug

--- src/logic/getQuote.js
+++ src/logic/getQuote.js
@@ -1,5 +1,6 @@
-export default async () => {
+const getQuote = async () => {
   const request = await fetch('https://random-math-quote-api.herokuapp.com/');
   const quote = await request.json();
   return quote;
 };
+export default getQuote;
